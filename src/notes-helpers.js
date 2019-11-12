679124bb83c4b14cbd1ba7f8f0173cb24860e180
Wesley Jacobs <w.jacobs.632@gmail.com>
2019-11-12 11:57:32 -0800

    initial commit after clone

---
+++ src/notes-helpers.js
@@ -0,0 +1,18 @@
+
+export const findFolder = (folders=[], folder_id) =>
+  folders.find(folder => folder.id == folder_id)
+
+export const findNote = (notes, note_id) => {
+  notes.find(note => note.id == note_id)
+}
+  
+ // notes.find(note => note.id === note_id)
+
+export const getNotesForFolder = (notes=[], folder_id) => (
+  (!folder_id)
+    ? notes
+    : notes.filter(note => note.folder_id === folder_id)
+)
+
+export const countNotesForFolder = (notes=[], folder_id) =>
+  notes.filter(note => note.folder_id === folder_id).length
